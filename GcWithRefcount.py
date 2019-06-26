8ef51058fe3260563e2f6339d7d71ab3a9d253e1
hyoriezmann <reddream09@naver.com>
2019-06-26 18:07:28 +0900

    Garbage collection with weakref

---
+++ GcWithRefcount.py
@@ -0,0 +1,14 @@
+import weakref
+
+s1 = {1,2,3}
+s2 = s1
+
+def bye():
+    print("Gone with the wind...")
+
+ender = weakref.finalize(s1, bye)
+ender.alive
+del s1
+ender.alive
+s2 = 'Steven Gerrard'
+ender.alive
